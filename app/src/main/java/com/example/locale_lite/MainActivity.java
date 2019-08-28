cd41e11d8084851e7bd7415038a6251c23d49c43
saketmht <mahtosaket04@gmail.com>
2019-08-28 23:27:37 +0530

    Initial commit

--- app/src/main/java/com/example/locale_lite/MainActivity.java
+++ app/src/main/java/com/example/locale_lite/MainActivity.java
@@ -1,52 +1,14 @@
 package com.example.locale_lite;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.viewpager.widget.ViewPager;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.RadioButton;
-import android.widget.RadioGroup;
-import android.widget.Spinner;
 
-import com.google.android.gms.maps.CameraUpdateFactory;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
-
-public class MainActivity extends AppCompatActivity{
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        ViewPager viewPager = (ViewPager) findViewById(R.id.startPager);
-        ImageAdapter adapter = new ImageAdapter(this);
-        viewPager.setAdapter(adapter);
-
-        Button createNew = (Button) findViewById(R.id.createnew);
-        createNew.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, CreateAccount.class);
-                startActivity(intent);
-            }
-        });
-        Button logIn = (Button) findViewById(R.id.login);
-        logIn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent i2 = new Intent(MainActivity.this, Login.class);
-                startActivity(i2);
-            }
-        });
     }
 }
-
