240ec84b71b31439d0d8be5f091298e90f4aa9dc
saketmht <mahtosaket04@gmail.com>
2019-09-06 13:40:54 +0530

    GetStarted Activity

---
+++ app/src/main/java/com/example/locale_lite/ImageAdapter.java
@@ -0,0 +1,43 @@
+package com.example.locale_lite;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.viewpager.widget.PagerAdapter;
+
+public class ImageAdapter extends PagerAdapter {
+
+    private Context mContext;
+    private int[] imageIds = new int[] {R.drawable.start1,R.drawable.start2,R.drawable.start3};
+
+    ImageAdapter(Context context){
+        mContext = context;
+    }
+    @Override
+    public int getCount() {
+        return imageIds.length;
+    }
+
+    @Override
+    public boolean isViewFromObject(@NonNull View view, @NonNull Object object) {
+        return view == object;
+    }
+
+    @NonNull
+    @Override
+    public Object instantiateItem(@NonNull ViewGroup container, int position) {
+        ImageView imageView = new ImageView(mContext);
+        imageView.setScaleType(ImageView.ScaleType.CENTER_INSIDE);
+        imageView.setImageResource(imageIds[position]);
+        container.addView(imageView, 0);
+        return imageView;
+    }
+
+    @Override
+    public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
+        container.removeView((ImageView) object);
+    }
+}
