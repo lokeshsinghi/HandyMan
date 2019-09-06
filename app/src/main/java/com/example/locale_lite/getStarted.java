ca15417d775fe9cccf7581d770b04f60d000e13e
saketmht <mahtosaket04@gmail.com>
2019-09-06 11:11:01 +0530

    Logo,startpage

--- app/src/main/java/com/example/locale_lite/getStarted.java
+++ app/src/main/java/com/example/locale_lite/getStarted.java
@@ -1,37 +1,7 @@
 package com.example.locale_lite;
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
 
-import com.google.android.material.textfield.TextInputEditText;
-
-public class getStarted extends AppCompatActivity {
-
-    Button createNew;
-    @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.get_started);
-
-        ViewPager viewPager = (ViewPager) findViewById(R.id.startPager);
-        ImageAdapter adapter = new ImageAdapter(this);
-        viewPager.setAdapter(adapter);
-
-        createNew = (Button) findViewById(R.id.createnew);
-        createNew.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getStarted.this, CreateAccount.class);
-                startActivity(intent);
-            }
-        });
-    }
+public class getStarted {
+    
 }
-
-
