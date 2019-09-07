1db91b20619965366aba948d6256bddf9c4bec46
lokeshsinghi <yourGitHub@email.com>
2019-09-07 11:53:35 +0530

    changed hello wirld

--- app/src/main/java/com/example/locale_lite/Login.java
+++ app/src/main/java/com/example/locale_lite/Login.java
@@ -1,115 +1,30 @@
 package com.example.locale_lite;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ProgressBar;
+import android.widget.Spinner;
 import android.widget.TextView;
-import android.widget.Toast;
 
-import com.firebase.client.Firebase;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
 public class Login extends AppCompatActivity  {
 
     TextView signUp;
-    int p=0,q=0;
-    EditText pNum;
-    TextView useEmail;
-    ProgressBar pbar;
     Button logIn;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        pNum = findViewById(R.id.phonenum);
         logIn =(Button) findViewById(R.id.login);
-        pbar = findViewById(R.id.loading);
-
         logIn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                final String phone = pNum.getText().toString();
-                if (pNum.getText().toString().length()<10)
-                    pNum.setError("Enter valid Phone Number");
-                else {
-                    pbar.setVisibility(View.VISIBLE);
-                    FirebaseDatabase.getInstance().getReference().child("Customers")
-                            .addListenerForSingleValueEvent(new ValueEventListener() {
-                                @Override
-                                public void onDataChange(DataSnapshot dataSnapshot) {
-                                    for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
-                                        pbar.setVisibility(View.GONE);
-                                        Customers c = snapshot.getValue(Customers.class);
-                                        if(c.getPhonenum().equals(phone)) {
-                                            p=1;
-                                            Intent intent = new Intent(Login.this, Main2Activity.class);
-                                            intent.putExtra("phonenumber", "+91" + phone);
-                                            startActivity(intent);
-                                            break;
-                                        }
-                                        else
-                                            p=0;
-                                    }
-                                }
-                                @Override
-                                public void onCancelled(DatabaseError databaseError) {
-                                }
-                            });
-
-
-
-
-                    FirebaseDatabase.getInstance().getReference().child("ServiceProviders")
-                            .addListenerForSingleValueEvent(new ValueEventListener() {
-                                @Override
-                                public void onDataChange(DataSnapshot dataSnapshot) {
-                                    for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
-                                        ServiceProviders c = snapshot.getValue(ServiceProviders.class);
-
-                                        if(c.getPhonenum().toString().equals(phone)) {
-                                            q=1;
-                                            pbar.setVisibility(View.GONE);
-                                            Intent intent = new Intent(Login.this, OTP.class);
-                                            intent.putExtra("phonenumber","+91"+phone);
-                                            startActivity(intent);
-                                            break;
-                                        }
-                                        else
-                                            q=0;
-
-                                    }
-                                    if(p==0 && q==0){
-                                        pbar.setVisibility(View.GONE);
-                                        Toast.makeText(Login.this, "Mobile Number not registered. Sign up", Toast.LENGTH_SHORT).show();
-                                    }
-                                }
-                                @Override
-                                public void onCancelled(DatabaseError databaseError) {
-                                }
-                            });
-                }
-            }
-
-        });
-
-        useEmail = (TextView) findViewById(R.id.email);
-        useEmail.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-
-                Intent in1 = new Intent(Login.this, EmailLogin.class);
-                startActivity(in1);
+                Intent intent = new Intent(Login.this, OTP.class);
+                startActivity(intent);
             }
         });
 
@@ -123,3 +38,5 @@ public class Login extends AppCompatActivity  {
         });
     }
 }
+
+
--- app/src/main/java/com/example/locale_lite/ProfileServiceProvider.java
+++ app/src/main/java/com/example/locale_lite/ProfileServiceProvider.java
@@ -1,293 +1,35 @@
 package com.example.locale_lite;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.ProgressDialog;
-import android.content.ContentResolver;
-import android.content.Context;
-import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.Color;
-import android.net.Uri;
 import android.os.Bundle;
-import android.provider.MediaStore;
 import android.view.View;
-
-import android.webkit.MimeTypeMap;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ImageView;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Spinner;
-import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.storage.FirebaseStorage;
-import com.google.firebase.storage.OnProgressListener;
-import com.google.firebase.storage.StorageReference;
-import com.google.firebase.storage.UploadTask;
-
-import java.io.IOException;
-
-public class ProfileServiceProvider extends AppCompatActivity implements View.OnClickListener  {
-
-
-
-    private static final int PICK_IMAGE_REQUEST = 234;
-
-    private Button buttonChoose1,buttonChoose2;
-    private Button buttonUpload1,buttonUpload2;
-    private Button next;
-    private ImageView imageView1,imageView2;
-    private String fname;
-    EditText Qualifications, Experience;
-    private String uploadId;
-    Uri downloadUrlPP, downloadUrlID;
-    String qualifications;
-    float experience;
-    private StorageReference storageReference;
-    private DatabaseReference databaseReference;
-    //a Uri object to store file path
-    private Uri filePath1;
-    static int count=0;
-
-
-
-
-
+public class ProfileServiceProvider extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_service_provider);
-
-        storageReference= FirebaseStorage.getInstance().getReference("uploads");
-        //getting buttons from layout
-        buttonChoose1 =  findViewById(R.id.chooser1);
-        buttonUpload1 =  findViewById(R.id.uploader1);
-        buttonChoose2 =  findViewById(R.id.chooser2);
-        buttonUpload2 =  findViewById(R.id.uploader2);
-        next = findViewById(R.id.profilenext_button);
-        Qualifications = findViewById(R.id.Qualifications);
-        Experience = findViewById(R.id.experience);
-        imageView1 = findViewById(R.id.imageView1);
-
-        //attaching listener
-        buttonChoose1.setOnClickListener(this);
-        buttonUpload1.setOnClickListener(this);
-        buttonChoose2.setOnClickListener(this);
-        buttonUpload2.setOnClickListener(this);
-
-        next.setOnClickListener(this);
-
-
-
+        Spinner categories = (Spinner) findViewById(R.id.categories);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.Categories, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        categories.setAdapter(adapter);
+        categories.setOnItemSelectedListener(this);
     }
 
-    //method to show file chooser
-    private void showFileChooser() {
-        Intent intent = new Intent();
-        intent.setType("image/*");
-        intent.setAction(Intent.ACTION_GET_CONTENT);
-        startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE_REQUEST);
-    }
-
-    //handling the image chooser activity result
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data != null && data.getData() != null) {
-            filePath1 = data.getData();
-            try {
-                Bitmap bitmap1 = MediaStore.Images.Media.getBitmap(getContentResolver(), filePath1);
-                imageView1.setImageBitmap(bitmap1);
+    public void onItemSelected(AdapterView<?> parent, View view, int i, long l) {
 
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-    }
-    private String getFileExtension(Uri uri){
-        ContentResolver cR = getContentResolver();
-        MimeTypeMap mime = MimeTypeMap.getSingleton();
-        return mime.getExtensionFromMimeType(cR.getType(uri));
     }
 
-
-    private void uploadFile1() {
-        //if there is a file to upload
-        if (filePath1 != null) {
-            //displaying a progress dialog while upload is going on
-            final ProgressDialog progressDialog = new ProgressDialog(this);
-            progressDialog.setTitle("Uploading");
-            progressDialog.show();
-
-            final StorageReference profileRef = storageReference.child(System.currentTimeMillis()+"."+getFileExtension(filePath1));
-            profileRef.putFile(filePath1)
-                    .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
-                        @Override
-                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
-                            //if the upload is successful
-                            //hiding the progress dialog
-
-                            progressDialog.dismiss();
-
-                            //and displaying a success toast
-                            profileRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
-                                @Override
-                                public void onSuccess(Uri uri) {
-                                    downloadUrlPP = uri;
-                                    count++;
-                                }
-
-                       });
-
-                    }})
-                    .addOnFailureListener(new OnFailureListener() {
-                        @Override
-                        public void onFailure(@NonNull Exception exception) {
-                            //if the upload is not successfull
-                            //hiding the progress dialog
-                            progressDialog.dismiss();
-
-                            //and displaying error message
-                            Toast.makeText(getApplicationContext(), exception.getMessage(), Toast.LENGTH_LONG).show();
-                        }
-                    })
-                    .addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
-                        @Override
-                        public void onProgress(UploadTask.TaskSnapshot taskSnapshot) {
-                            //calculating progress percentage
-                            double progress = 100.0 * (taskSnapshot.getBytesTransferred()/ taskSnapshot.getTotalByteCount());
-
-                            //displaying percentage in progress dialog
-                            progressDialog.setMessage("Uploaded " + ((int) progress) + "%...");
-
-                        }
-
-                    });
-        }
-        //if there is not any file
-        else {
-            //you can display an error toast
-            Toast.makeText(getApplicationContext(), "No file selected", Toast.LENGTH_SHORT).show();
-        }
-    }
-
-
-
-
-    private void uploadFile2() {
-        //if there is a file to upload
-        if (filePath1 != null) {
-            //displaying a progress dialog while upload is going on
-            final ProgressDialog progressDialog = new ProgressDialog(this);
-            progressDialog.setTitle("Uploading");
-            progressDialog.show();
-
-            final StorageReference profileRef = storageReference.child(System.currentTimeMillis()+"."+getFileExtension(filePath1));
-            profileRef.putFile(filePath1)
-                    .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
-                        @Override
-                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
-                            //if the upload is successful
-                            //hiding the progress dialog
-
-                            progressDialog.dismiss();
-
-                            //and displaying a success toast
-                            profileRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
-                                @Override
-                                public void onSuccess(Uri uri) {
-                                    downloadUrlID = uri;
-                                    //Do what you want with the url
-                                    count++;
-                                }
-
-                            });
-
-                        }})
-                    .addOnFailureListener(new OnFailureListener() {
-                        @Override
-                        public void onFailure(@NonNull Exception exception) {
-                            //if the upload is not successfull
-                            //hiding the progress dialog
-                            progressDialog.dismiss();
-
-                            //and displaying error message
-                            Toast.makeText(getApplicationContext(), exception.getMessage(), Toast.LENGTH_LONG).show();
-                        }
-                    })
-                    .addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
-                        @Override
-                        public void onProgress(UploadTask.TaskSnapshot taskSnapshot) {
-                            //calculating progress percentage
-                            double progress = 100.0 * (taskSnapshot.getBytesTransferred()/ taskSnapshot.getTotalByteCount());
-
-                            //displaying percentage in progress dialog
-                            progressDialog.setMessage("Uploaded " + ((int) progress) + "%...");
-
-                        }
-
-                    });
-        }
-        //if there is not any file
-        else {
-            //you can display an error toast
-            Toast.makeText(getApplicationContext(), "No file selected", Toast.LENGTH_SHORT).show();
-        }
-    }
     @Override
-    public void onClick(View view) {
-        if(view == next)
-        {
-
-            if(count<2)
-                Toast.makeText(ProfileServiceProvider.this, "Upload all required files",Toast.LENGTH_SHORT).show();
-            else if (Qualifications.getText().toString().trim().equalsIgnoreCase(""))
-                Qualifications.setError("Qualifications required");
-
-            else if (Experience.getText().toString().trim().equalsIgnoreCase(""))
-                Experience.setError("Years of Experience Required");
-            else {
-                Bundle bundle = getIntent().getExtras();
-                bundle.putString("downloadUrlID",downloadUrlID.toString());
-                bundle.putString("downloadUrlPP",downloadUrlPP.toString());
-                bundle.putString("qualifications",Qualifications.getText().toString());
-                bundle.putString("yearsOfExperience",Experience.getText().toString());
-                final String phonenumber = getIntent().getStringExtra("phonenumber");
-                Intent intent = new Intent(ProfileServiceProvider.this, SignupSP_OTP.class);
-                intent.putExtra("phonenumber", phonenumber);
-                intent.putExtras(bundle);
-                startActivity(intent);
-            }
-        }
-        //if the clicked button is choose
-        if (view == buttonChoose1) {
-            showFileChooser();
-        }
-        //if the clicked button is upload
-        else if (view == buttonUpload1) {
-            uploadFile1();
-        }
-        if (view == buttonChoose2) {
-            showFileChooser();
-        }
-        //if the clicked button is upload
-        else if (view == buttonUpload2) {
-            uploadFile2();
-        }
+    public void onNothingSelected(AdapterView<?> parent) {
 
     }
 }
-
-
-
-
 
