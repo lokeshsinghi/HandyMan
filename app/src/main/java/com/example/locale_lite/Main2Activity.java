ffb0e54d63830215ff9555b2311278c6d5b5b830
saketmht <mahtosaket04@gmail.com>
2019-09-10 03:56:05 +0530

    mainactivity2

--- app/src/main/java/com/example/locale_lite/Main2Activity.java
+++ app/src/main/java/com/example/locale_lite/Main2Activity.java
@@ -1,34 +1,31 @@
 package com.example.locale_lite;
 
 import android.os.Bundle;
-import android.widget.Button;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 public class Main2Activity extends AppCompatActivity {
-
-    Button locbut,passbut,reqbut,logbut,histbut;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
         BottomNavigationView navView = findViewById(R.id.nav_view);
-
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.navigation_home, R.id.navigation_notifications)
+                .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
-
-        locbut= (Button) findViewById(R.id.change_location);
-        passbut= (Button) findViewById(R.id.change_password);
-        reqbut= (Button) findViewById(R.id.request_sent);
-        logbut= (Button) findViewById(R.id.logout);
-        histbut= (Button) findViewById(R.id.service_history);
-
+        getSupportActionBar().hide();
     }
 
 }
