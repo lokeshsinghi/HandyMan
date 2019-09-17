8d1c4438d538e9ec8821c0f2ca4dd462a4cd23d6
Lokesh Raj Singhi <lokeshsinghi2000@gmail.com>
2019-09-18 03:05:06 +0530

    Update SP Profile Progress Bar kept

--- app/src/main/java/com/example/locale_lite/MapDirections.java
+++ app/src/main/java/com/example/locale_lite/MapDirections.java
@@ -1,6 +1,8 @@
 package com.example.locale_lite;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.location.Location;
 import android.os.Bundle;
 //import android.support.annotation.Nullable;
@@ -12,18 +14,24 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentActivity;
 
 import com.example.locale_lite.R;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.Polyline;
 import com.google.android.gms.maps.model.Marker;
 
 import com.google.android.gms.maps.model.PolylineOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -40,6 +48,7 @@ public class MapDirections extends FragmentActivity implements OnMapReadyCallbac
     Double spLat, spLng, cusLat, cusLng;
     Button getDirection;
     private Polyline currentPolyline;
+    private static final int REQUEST_CODE = 101;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -53,15 +62,26 @@ public class MapDirections extends FragmentActivity implements OnMapReadyCallbac
 //            }
 //        });
 
+        fetchLastLocation();
 
 
 
 
-
-        MapFragment mapFragment = (MapFragment) getFragmentManager()
-                .findFragmentById(R.id.mapNearBy);
-        mapFragment.getMapAsync(this);
+//        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+//                .findFragmentById(R.id.map);
+//        mapFragment.getMapAsync(MapDirections.this);
     }
+    private void fetchLastLocation() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_CODE);
+            return;
+        }
+
+                    SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                            .findFragmentById(R.id.map);
+                    mapFragment.getMapAsync(MapDirections.this);
+                }
 
 //    private String getRequestUrl(LatLng origin, LatLng destin) {
 //
@@ -85,15 +105,23 @@ public class MapDirections extends FragmentActivity implements OnMapReadyCallbac
         MarkerOptions markerOptions1 = new MarkerOptions();
         markerOptions1.position(spLocation);
         googleMap.addMarker(markerOptions1);
-
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(spLocation, 16));
 
         MarkerOptions markerOptions = new MarkerOptions();
         markerOptions.position(cusLocation);
         googleMap.addMarker(markerOptions);
-
         mMap.getUiSettings().setZoomControlsEnabled(true);
         mMap.setMyLocationEnabled(true);
     }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_CODE:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    fetchLastLocation();
+                }
+                break;
+        }}
 
 //    private String getUrl(LatLng origin, LatLng dest, String directionMode) {
 //        // Origin of route
